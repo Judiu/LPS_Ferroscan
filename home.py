5f14e4f02da935639fd4320cf93f2a83b47253d2
Fredy Piraquive <40009527+Judiu@users.noreply.github.com>
2024-02-17 22:46:09 -0500

    Update home.py

--- home.py
+++ home.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import json
 
 
 def leer_csv():
@@ -10,8 +11,42 @@ def leer_csv():
         for fila in lector_csv:
             #print(', '.join(fila))
             lista_datos.append(fila)
-    datos_fila = lista_datos[3]
-    print (datos_fila)
+    solo_datos = []
+
+    for fila in lista_datos[3:]:
+        solo_datos.append(fila)
+        datos_separados = [item.split(';') for item in fila]
+        # item = datos_separados(0)
+        # print (item)
+        # distancia = datos_separados[1]
+        # print (distancia)
+        # cubierta = datos_separados[2]
+        # print (cubierta)
+        # diametro = datos_separados[3]
+        # print (diametro)
+        # overlay = datos_separados[4]    
+        # print (overlay)
+        # realdiametro = datos_separados[5]
+        # print (realdiametro)
+        # calidad = datos_separados[6]
+        # print (calidad)
+        # tipo = datos_separados[7]
+        # print (tipo)
+        # trazo = datos_separados[8]
+        # print (trazo)
+
+    print (datos_separados)
+    # Do something with the JSON data
+    titulos = solo_datos[0]
+    datos_con_titulos = []
+    for fila in solo_datos[1:]:
+        datos_con_titulos.append(dict(zip(titulos, fila)))
+        
+        # Do something with the JSON data
+    json_data = json.dumps(solo_datos)
+        
+    with open('data.json', 'w') as file:
+        json.dump(solo_datos, file)
     return lista_datos
 
 def obtener_nombres_archivos(directorio = "./csvdata/"):
